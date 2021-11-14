51b7609a789ca9e0253a06088091bc0dba48682d
Hazar Belge <hazarbelge@gmail.com>
2021-11-14 15:52:07 +0300

    Configure main.py

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
 from modules.csv import init_csv
+from modules.data_set.achievements import achievements
+from modules.data_set.age_rating import age_rating
+from modules.data_set.english_support import english_support
 
-init_csv.initialize_data_set()
-init_csv.configure_table()
+
+def initialize_analysis():
+    ds = init_csv.initialize_data_set()
+    init_csv.configure_rc_params()
+    return ds
+
+
+def achievements_analysis():
+    achievements.show_pie_chart(data_set)
+
+
+def age_rating_analysis():
+    age_rating.show_pie_chart_has_age_rating(data_set)
+    age_rating.show_pie_chart_with_age_rating(data_set)
+
+
+def english_support_analysis():
+    english_support.show_pie_chart(data_set)
+
+
+data_set = initialize_analysis()
+
+achievements_analysis()
+age_rating_analysis()
+english_support_analysis()
