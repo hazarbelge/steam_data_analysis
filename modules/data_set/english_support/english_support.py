e8f5f6a99be8fb1a1aae7d5a72133849f7f21358
Hazar Belge <hazarbelge@gmail.com>
2021-11-14 15:49:58 +0300

    Add english_support pie chart

---
+++ modules/data_set/english_support/english_support.py
@@ -0,0 +1,25 @@
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+
+def show_pie_chart(data_set):
+    english_dict = dict(data_set.english.value_counts())
+    labels = 'Non-English', 'English'
+    sizes = [english_dict[0], english_dict[1]]
+    colors = ['blue', 'red']
+    explode = (0, 0.3)
+
+    with sns.color_palette("colorblind"):
+        plt.pie(
+            sizes,
+            explode=explode,
+            labels=labels,
+            colors=colors,
+            autopct='%1.1f%%',
+            shadow=False,
+            startangle=45,
+        )
+
+    plt.title('English Support')
+    plt.axis('equal')
+    plt.show()
