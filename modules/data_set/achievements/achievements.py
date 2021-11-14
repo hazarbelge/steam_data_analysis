20adabd650431225bc4039a9516166790dd6102d
Hazar Belge <hazarbelge@gmail.com>
2021-11-14 15:49:46 +0300

    Add has_achievements pie chart

---
+++ modules/data_set/achievements/achievements.py
@@ -0,0 +1,25 @@
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+
+def show_pie_chart(data_set):
+    achievement_dict = dict(data_set.has_achievements.value_counts())
+    labels = 'Has Achievements', 'No Achievements'
+    sizes = [achievement_dict[0], achievement_dict[1]]
+    colors = ['lightskyblue', 'gold']
+    explode = (0, 0.1)
+
+    with sns.color_palette("colorblind"):
+        plt.pie(
+            sizes,
+            explode=explode,
+            labels=labels,
+            colors=colors,
+            autopct='%1.1f%%',
+            shadow=False,
+            startangle=45,
+        )
+
+    plt.title('Achievements')
+    plt.axis('equal')
+    plt.show()
